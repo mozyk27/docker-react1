cdb801042d24afe8ca345be11c746ea90370dc4c
Jimmy Kung <Mozy@Jimmys-MacBook-Pro.local>
2019-03-21 08:12:15 -0500

    initial commit

---
+++ build/precache-manifest.ee4636bee1717d3faa9a0b7d89e7dfce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3b125fa9fa0942f8c91a",
+    "url": "/static/js/main.10993aea.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "3b125fa9fa0942f8c91a",
+    "url": "/static/css/main.76acfb9f.chunk.css"
+  },
+  {
+    "revision": "a3391c2c40ca15843732f6e788469840",
+    "url": "/index.html"
+  }
+];
